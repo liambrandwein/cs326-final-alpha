5edec36796a388706b89a5fcca770f58d1b9fcad
liambrandwein <liam.brandwein@gmail.com>
2021-11-06 14:58:31 -0400

    User sub data

--- src/server/handle_reqs.js
+++ src/server/handle_reqs.js
@@ -1,21 +1,21 @@
 'use strict';
-import { createRequire } from 'module';
 let fs = require('fs');
 
-export function getDataBase(data_type){
+function getDataBase(data_type){
     const supported_data_types = ['user_data', 'creator_data', 
                                 'user_watch_hist_data', 'user_sub_data' ];
     if (!data_type in supported_data_types){
         console.alert("BUG: data not supported");
         return {};
     }
-    const filename = './data/' + data_type + '.json';
+    const filename = 'src/server/data/' + data_type + '.json';
+    // Add if file exists
     const str = fs.readFileSync(filename, { encoding: 'utf8' });
     return JSON.parse(str);
 }
 
 // verify if user's supplied account and password match the data in the database
-export function loginAccount(req, res){
+function loginAccount(req, res){
     const user_data = getDataBase('user_data');
     const body = req.body;
     if (body.id  in user_data || user_data[body.id] === body.pass){
@@ -35,13 +35,13 @@ export function loginAccount(req, res){
 }
 
 // check if user's account is already in the database. 
-export function checkAccountExist(req){
+function checkAccountExist(req){
     const user_data = getDataBase('user_data');
     const body = req.body;
     return body.id in user_data;
 }
 // check if new account can be created
-export function createAccount(req, res){
+function createAccount(req, res){
     const user_data = getDataBase('user_data');
     const body = req.body;
     if (checkAccountExist(req)){
@@ -52,7 +52,7 @@ export function createAccount(req, res){
     }
     else{
         user_data[body.id] = body.pass;
-        fs.writeFileSync('./data/user_data.json', JSON.stringify(user_data));
+        fs.writeFileSync('src/server/data/user_data.json', JSON.stringify(user_data));
         res.send({
             status: 'success',
             msg: 'create account success'
@@ -61,16 +61,16 @@ export function createAccount(req, res){
 }
 
 // add a new creator to the user's sub list
-export function addUserSub(req, res){
+function addUserSub(req, res){
     const user_sub_data = getDataBase('user_sub_data');
     const body = req.body;
     if (body.id in user_sub_data){
-        user_sub_data[id].add(body.sub_id);
+        user_sub_data[body.id].push(body.creator_id);
     }
-    else{
-        user_sub_data[id] = new Set([body.sub_id]);
+    else {
+        user_sub_data[body.id] = Array(body.creator_id);
     }
-    fs.writeFileSync('./data/user_sub_data.json', JSON.stringify(user_sub_data));
+    fs.writeFileSync('src/server/data/user_sub_data.json', JSON.stringify(user_sub_data));
     res.send({
         status: 'success',
         msg: 'add sub success'
@@ -79,7 +79,7 @@ export function addUserSub(req, res){
 
 // remove a creator from the user's sub list
 // user_sub_data[id] = set
-export function removeUserSub(req, res){
+function removeUserSub(req, res){
     const user_sub_data = getDataBase('user_sub_data');
     const body = req.body;
     if (body.id in user_sub_data){
@@ -95,10 +95,10 @@ export function removeUserSub(req, res){
             msg: 'user_sub_data[id] does not exist. Delete creator is NULL.'
         });
     }
-    fs.writeFileSync('./data/user_sub_data.json', JSON.stringify(user_sub_data));
+    fs.writeFileSync('src/server/data/user_sub_data.json', JSON.stringify(user_sub_data));
 }
 
-export function updateWatchHist(req, res){
+function updateWatchHist(req, res){
     const user_watch_hist_data = getDataBase('user_watch_hist_data');
     const body = req.body;
     if (body.id in user_watch_hist_data){
@@ -107,27 +107,27 @@ export function updateWatchHist(req, res){
     else{
         user_watch_hist_data[id] = [body.creator_id];
     }
-    fs.writeFileSync('./data/user_watch_hist_data.json', JSON.stringify(user_watch_hist_data));
+    fs.writeFileSync('src/server/data/user_watch_hist_data.json', JSON.stringify(user_watch_hist_data));
     res.send({
         status: 'success',
         msg: 'update watch hist success'
     });
 }
 
-export function clearWatchHist(req, res){
+function clearWatchHist(req, res){
     const user_watch_hist_data = getDataBase('user_watch_hist_data');
     const body = req.body;
     if (body.id in user_watch_hist_data){
         user_watch_hist_data[id] = [];
     }
-    fs.writeFileSync('./data/user_watch_hist_data.json', JSON.stringify(user_watch_hist_data));
+    fs.writeFileSync('src/server/data/user_watch_hist_data.json', JSON.stringify(user_watch_hist_data));
     res.send({
         status: 'success',
         msg: 'clear watch hist success'
     });
 }
 // creator_data[creator_id] = [ {platform: ..., url: ...}, ...]
-export function addCreator(req, res){
+function addCreator(req, res){
     const creator_data = getDataBase('creator_data');
     const body = req.body;
     if (body.creator_id in creator_data){
@@ -136,9 +136,21 @@ export function addCreator(req, res){
     else{
         creator_data[body.creator_id] = new Set([{'platform': body.platform, 'url': body.url}]);
     }
-    fs.writeFileSync('./data/creator_data.json', JSON.stringify(creator_data));
+    fs.writeFileSync('src/server/data/creator_data.json', JSON.stringify(creator_data));
     res.send({
         status: 'success',
         msg: 'add creator success'
     });
+}
+
+module.exports = {
+    getDataBase,
+    loginAccount,
+    checkAccountExist,
+    createAccount,
+    addUserSub,
+    removeUserSub,
+    updateWatchHist,
+    clearWatchHist,
+    addCreator
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const handle = require('./handle_reqs');
 const app = express();
 const PORT = 8080;
 
@@ -43,42 +44,9 @@ app.get('/results', (req, res) => {
     res.sendFile('searchResult.html', {root: 'src'});
 })
 
-app.post('/createaccount', (req, res) => {   
-    // {id: 'email@aol.com', password: 'asdfj42fg'} 
-    const body = req.body;
-    
-    if (!body.id || !body.pass) {
-        res.status(400).send({
-            result: 'Error: need email and password'
-        });
-    }
+app.post('/createaccount', handle.createAccount);
 
-    res.status(200).send({
-        result: `Account creation successful! Email is ${body.id} and pass is ${body.pass}`
-    });
-});
-
-app.post('/addcreator', (req, res) => {
-    // {hikaru: [{platform: youtube, url: 'youtube.com/ksdajflkjsa.com'}, {platform...}...]}
-    const body = req.body;
-    // Add function to add this to subscribed things
-    const name = Object.keys(body)[0];
-
-    /*
-    if req.params.hasOwnProperty('watch-history') <--- add to watch-history
-    else if req.params.hasOwnProperty('sub-manager') <--- add to subscribed creators
-    */
-
-    if (!body[name]) {
-        res.status(400).send({
-            result: 'Error: need creator name and platforms'
-        });
-    }
-
-    res.status(200).send({
-        result: `Creator added. Name is ${name} and platforms are ${body[name]}`
-    });
-});
+app.post('/addusersub', handle.addUserSub);
 
 app.delete('/removecreator', (req, res) => {
     /*
