a04916830a1218ee7ab5a6120a8b98012f45e48b
liambrandwein <liam.brandwein@gmail.com>
2021-11-05 22:30:11 -0400

    Adding to backend

--- src/server/index.js
+++ src/server/index.js
@@ -16,7 +16,8 @@ app.get('/test', (req, res) => {
     });
 });
 
-app.post('/createaccount', (req, res) => {    
+app.post('/createaccount', (req, res) => {   
+    // {id: 'email@aol.com', password: 'asdfj42fg'} 
     const body = req.body;
     
     if (!body.id || !body.pass) {
@@ -27,5 +28,27 @@ app.post('/createaccount', (req, res) => {
 
     res.status(200).send({
         result: `Account creation successful! Email is ${body.id} and pass is ${body.pass}`
+    });
+});
+
+app.post('/addcreator', (req, res) => {
+    // {hikaru: [{platform: youtube, url: 'youtube.com/ksdajflkjsa.com'}, {platform...}...]}
+    const body = req.body;
+    // Add function to add this to subscribed things
+    const name = Object.keys(body)[0];
+
+    /*
+    if req.params.hasOwnProperty('watch-history') <--- add to watch-history
+    else if req.params.hasOwnProperty('sub-manager') <--- add to subscribed creators
+    */
+
+    if (!body[name]) {
+        res.status(400).send({
+            result: 'Error: need creator name and platforms'
+        });
+    }
+
+    res.status(200).send({
+        result: `Creator added. Name is ${name} and platforms are ${body[name]}`
     });
 });
