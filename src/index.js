4b4bb9e43fed71e542209914108426314451a8a4
aaratsatyavolu <38267657+aaratsatyavolu@users.noreply.github.com>
2021-11-06 19:26:00 -0400

    Update index.js
    Initial go at dynamic loading - need to add in get request to JSON

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+//function for dynamically loading creator content
 
+async function loadCreator() {
+  const newDiv = document.createElement("card"); //creates card element using div notation from HTML file
+  const cardContent = 
+        
+  const currentDiv = document.getElementById("card1");
+  document.body.insertBefore(newDiv, currentDiv); 
+  
+}
