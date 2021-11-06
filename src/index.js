5aec398e2e81be5a4b4a0f8c18c86d475b1ec178
aaratsatyavolu <38267657+aaratsatyavolu@users.noreply.github.com>
2021-11-06 19:27:14 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //function for dynamically loading creator content
 
 async function loadCreator() {
-  const newDiv = document.createElement("card"); //creates card element using div notation from HTML file
+  const newDiv = document.createElement("card"); //creates card element using div name from index.html
   const cardContent = 
         
   const currentDiv = document.getElementById("card1");
