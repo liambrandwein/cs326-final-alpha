21c954672178a3ad62f7256771e0bad4d4b46408
liambrandwein <liam.brandwein@gmail.com>
2021-11-06 22:51:15 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 //function for dynamically loading creator content
 if (!window.localStorage.getItem('username')) {
-  window.location.href = 'http://localhost:8080/signin';
+  window.location.href = 'http://localhost:3000/signin';
 }
 
 async function loadCreator() {
